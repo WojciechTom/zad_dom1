d685ca598e3dd95306b6bf0ffb73e58d07a640d0
Wojciech T <hadwao2@gmail.com>
2018-09-11 00:15:08 +0200

    zmiany 2

--- src/zad_dom1.java
+++ src/zad_dom1.java
@@ -2,6 +2,8 @@ public class zad_dom1 {
     public static void main(String[] args){
         System.out.println("Uczę");
         System.out.println("się");
-        System.out.println("programować");
+        System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
     }
 }
